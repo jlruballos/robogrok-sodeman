51da38b6dfa5dde4844e228e598e92623be4f702
jlruballos <124950155+jlruballos@users.noreply.github.com>
2023-02-14 21:47:26 -0800

    Add files via upload

---
+++ PSoC/Control.cydsn/Motor2.h
@@ -0,0 +1,165 @@
+/*******************************************************************************
+* File Name: Motor2.h  
+* Version 2.20
+*
+* Description:
+*  This file contains Pin function prototypes and register defines
+*
+* Note:
+*
+********************************************************************************
+* Copyright 2008-2015, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions, 
+* disclaimers, and limitations in the end user license agreement accompanying 
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#if !defined(CY_PINS_Motor2_H) /* Pins Motor2_H */
+#define CY_PINS_Motor2_H
+
+#include "cytypes.h"
+#include "cyfitter.h"
+#include "cypins.h"
+#include "Motor2_aliases.h"
+
+/* APIs are not generated for P15[7:6] */
+#if !(CY_PSOC5A &&\
+	 Motor2__PORT == 15 && ((Motor2__MASK & 0xC0) != 0))
+
+
+/***************************************
+*        Function Prototypes             
+***************************************/    
+
+/**
+* \addtogroup group_general
+* @{
+*/
+void    Motor2_Write(uint8 value);
+void    Motor2_SetDriveMode(uint8 mode);
+uint8   Motor2_ReadDataReg(void);
+uint8   Motor2_Read(void);
+void    Motor2_SetInterruptMode(uint16 position, uint16 mode);
+uint8   Motor2_ClearInterrupt(void);
+/** @} general */
+
+/***************************************
+*           API Constants        
+***************************************/
+/**
+* \addtogroup group_constants
+* @{
+*/
+    /** \addtogroup driveMode Drive mode constants
+     * \brief Constants to be passed as "mode" parameter in the Motor2_SetDriveMode() function.
+     *  @{
+     */
+        #define Motor2_DM_ALG_HIZ         PIN_DM_ALG_HIZ
+        #define Motor2_DM_DIG_HIZ         PIN_DM_DIG_HIZ
+        #define Motor2_DM_RES_UP          PIN_DM_RES_UP
+        #define Motor2_DM_RES_DWN         PIN_DM_RES_DWN
+        #define Motor2_DM_OD_LO           PIN_DM_OD_LO
+        #define Motor2_DM_OD_HI           PIN_DM_OD_HI
+        #define Motor2_DM_STRONG          PIN_DM_STRONG
+        #define Motor2_DM_RES_UPDWN       PIN_DM_RES_UPDWN
+    /** @} driveMode */
+/** @} group_constants */
+    
+/* Digital Port Constants */
+#define Motor2_MASK               Motor2__MASK
+#define Motor2_SHIFT              Motor2__SHIFT
+#define Motor2_WIDTH              1u
+
+/* Interrupt constants */
+#if defined(Motor2__INTSTAT)
+/**
+* \addtogroup group_constants
+* @{
+*/
+    /** \addtogroup intrMode Interrupt constants
+     * \brief Constants to be passed as "mode" parameter in Motor2_SetInterruptMode() function.
+     *  @{
+     */
+        #define Motor2_INTR_NONE      (uint16)(0x0000u)
+        #define Motor2_INTR_RISING    (uint16)(0x0001u)
+        #define Motor2_INTR_FALLING   (uint16)(0x0002u)
+        #define Motor2_INTR_BOTH      (uint16)(0x0003u) 
+    /** @} intrMode */
+/** @} group_constants */
+
+    #define Motor2_INTR_MASK      (0x01u) 
+#endif /* (Motor2__INTSTAT) */
+
+
+/***************************************
+*             Registers        
+***************************************/
+
+/* Main Port Registers */
+/* Pin State */
+#define Motor2_PS                     (* (reg8 *) Motor2__PS)
+/* Data Register */
+#define Motor2_DR                     (* (reg8 *) Motor2__DR)
+/* Port Number */
+#define Motor2_PRT_NUM                (* (reg8 *) Motor2__PRT) 
+/* Connect to Analog Globals */                                                  
+#define Motor2_AG                     (* (reg8 *) Motor2__AG)                       
+/* Analog MUX bux enable */
+#define Motor2_AMUX                   (* (reg8 *) Motor2__AMUX) 
+/* Bidirectional Enable */                                                        
+#define Motor2_BIE                    (* (reg8 *) Motor2__BIE)
+/* Bit-mask for Aliased Register Access */
+#define Motor2_BIT_MASK               (* (reg8 *) Motor2__BIT_MASK)
+/* Bypass Enable */
+#define Motor2_BYP                    (* (reg8 *) Motor2__BYP)
+/* Port wide control signals */                                                   
+#define Motor2_CTL                    (* (reg8 *) Motor2__CTL)
+/* Drive Modes */
+#define Motor2_DM0                    (* (reg8 *) Motor2__DM0) 
+#define Motor2_DM1                    (* (reg8 *) Motor2__DM1)
+#define Motor2_DM2                    (* (reg8 *) Motor2__DM2) 
+/* Input Buffer Disable Override */
+#define Motor2_INP_DIS                (* (reg8 *) Motor2__INP_DIS)
+/* LCD Common or Segment Drive */
+#define Motor2_LCD_COM_SEG            (* (reg8 *) Motor2__LCD_COM_SEG)
+/* Enable Segment LCD */
+#define Motor2_LCD_EN                 (* (reg8 *) Motor2__LCD_EN)
+/* Slew Rate Control */
+#define Motor2_SLW                    (* (reg8 *) Motor2__SLW)
+
+/* DSI Port Registers */
+/* Global DSI Select Register */
+#define Motor2_PRTDSI__CAPS_SEL       (* (reg8 *) Motor2__PRTDSI__CAPS_SEL) 
+/* Double Sync Enable */
+#define Motor2_PRTDSI__DBL_SYNC_IN    (* (reg8 *) Motor2__PRTDSI__DBL_SYNC_IN) 
+/* Output Enable Select Drive Strength */
+#define Motor2_PRTDSI__OE_SEL0        (* (reg8 *) Motor2__PRTDSI__OE_SEL0) 
+#define Motor2_PRTDSI__OE_SEL1        (* (reg8 *) Motor2__PRTDSI__OE_SEL1) 
+/* Port Pin Output Select Registers */
+#define Motor2_PRTDSI__OUT_SEL0       (* (reg8 *) Motor2__PRTDSI__OUT_SEL0) 
+#define Motor2_PRTDSI__OUT_SEL1       (* (reg8 *) Motor2__PRTDSI__OUT_SEL1) 
+/* Sync Output Enable Registers */
+#define Motor2_PRTDSI__SYNC_OUT       (* (reg8 *) Motor2__PRTDSI__SYNC_OUT) 
+
+/* SIO registers */
+#if defined(Motor2__SIO_CFG)
+    #define Motor2_SIO_HYST_EN        (* (reg8 *) Motor2__SIO_HYST_EN)
+    #define Motor2_SIO_REG_HIFREQ     (* (reg8 *) Motor2__SIO_REG_HIFREQ)
+    #define Motor2_SIO_CFG            (* (reg8 *) Motor2__SIO_CFG)
+    #define Motor2_SIO_DIFF           (* (reg8 *) Motor2__SIO_DIFF)
+#endif /* (Motor2__SIO_CFG) */
+
+/* Interrupt Registers */
+#if defined(Motor2__INTSTAT)
+    #define Motor2_INTSTAT            (* (reg8 *) Motor2__INTSTAT)
+    #define Motor2_SNAP               (* (reg8 *) Motor2__SNAP)
+    
+	#define Motor2_0_INTTYPE_REG 		(* (reg8 *) Motor2__0__INTTYPE)
+#endif /* (Motor2__INTSTAT) */
+
+#endif /* CY_PSOC5A... */
+
+#endif /*  CY_PINS_Motor2_H */
+
+
+/* [] END OF FILE */
